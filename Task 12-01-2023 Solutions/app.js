9efaddee7369d4ca4c4786571a72a1ad5165ba98
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-12 17:11:08 +0500

    12-01-2023

---
+++ Task 12-01-2023 Solutions/app.js
@@ -0,0 +1,42 @@
+// let num1=+prompt("Enter the first number")
+// let num2=+prompt("Enter the second number")
+
+// let sum=num1+num2
+
+// if(num1===num2){
+//     alert(3*sum)
+// }
+
+// else{
+//     alert(sum)
+// }
+
+// let num1=+prompt("Enter the first number")
+// let num2=+prompt("Enter the second number")
+// let num3=+prompt("Enter the third number")
+
+// if(num1===num2&&num2===num3){
+//     alert("30")
+// }
+// else if(num1===num2||num2===num3||num1===num3){
+//     alert("40")
+// }
+// else{
+//     alert("20")
+// }
+
+// let num1=+prompt("Enter the first number")
+// let num2=+prompt("Enter the second number")
+
+// let diff1=100-num1
+// let diff2=100-num2
+
+// if(diff1>diff2){
+//     alert(num2)
+// }
+// else if(diff1<diff2){
+//     alert(num1)
+// }
+// else{
+//     alert("Invalid")
+// }
